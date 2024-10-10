2e09763c7f523d935d19e4290d2d3ebf9c6d93ec
Juan Camilo Mejía Montoya <j66cam@gmail.com>
2024-10-10 16:50:18 -0500

    chore(TestRunner): Small comments

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -5,8 +5,9 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/test/java/features"},
-		glue = {"seleniumgluecode", "hooks"},
+@CucumberOptions(
+		features = {"src/test/java/features"},                      // Where the features are located
+		glue = {"seleniumgluecode", "hooks"},                       // Where the steps/methods of the feature are located
 //		plugin = {"json:target/cucumber_report.json"},				// For a Json report results
 //		plugin = {"pretty", "json:target/cucumber_report.json"},	// For a Json report results AND the scenario is displayed on the console with "pretty"
 		plugin = {"html:target/cucumber.html"},                     // For a more friendly cucumber report
