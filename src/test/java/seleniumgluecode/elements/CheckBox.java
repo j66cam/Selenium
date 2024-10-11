de1a15c66547f7f43681f13904254f2af7c8bc60
Juan Camilo Mejía Montoya <j66cam@gmail.com>
2024-10-11 17:12:14 -0500

    chore(CheckBox): Small changes

--- src/test/java/seleniumgluecode/elements/CheckBox.java
+++ src/test/java/seleniumgluecode/elements/CheckBox.java
@@ -18,9 +18,7 @@ public class CheckBox {
 		WebDriver driver = Browser.startBrowser();
 		Home_Page home = new Home_Page(driver);
 		checkBox_page = new CheckBox_Page(driver);
-//		Thread.sleep(5000);
 		home.goToPage(Home_Page.elements);
-//		Thread.sleep(5000);
 		checkBox_page.clickCheckBoxTab();
 	}
 
@@ -32,6 +30,5 @@ public class CheckBox {
 	@Then("The user should see the selected {string}")
 	public void theUserShouldSeeTheSelected(String finalItem) {
 		assertEquals(finalItem.toLowerCase(), checkBox_page.getOutputSelectedLb());
-		System.out.println("ESO HPTA!!!!");
 	}
 }
