3f220c94b5f8f7eac0f2801cdc1663c5b1a36b4e
Juan Camilo Mejía Montoya <j66cam@gmail.com>
2024-10-30 15:00:49 -0500

    feat(Cluecumber_Report): Adding the plugin on pom.xml file and configuration for the report on TestRunner class

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -10,7 +10,8 @@ import org.junit.runner.RunWith;
 		glue = {"seleniumgluecode", "hooks"},                       // Where the steps/methods of the feature are located
 //		plugin = {"json:target/cucumber_report.json"},				// For a Json report results
 //		plugin = {"pretty", "json:target/cucumber_report.json"},	// For a Json report results AND the scenario is displayed on the console with "pretty"
-		plugin = {"html:target/cucumber.html"},                     // For a more friendly cucumber report
+//		plugin = {"html:target/cucumber.html"},                     // For a more friendly cucumber report
+		plugin = {"json:target/cucumber-reports/Cucumber.json"},    // For the Cluecumber Report
 		dryRun = false,                                             // To check whether all feature file steps have corresponding step definitions
 		monochrome = false                                         // if we want console output from Cucumber in a readable format
 //		tags = "@"                                                  // If we would like to execute the tests by tags in the terminal (mvn test), we must comment this line
