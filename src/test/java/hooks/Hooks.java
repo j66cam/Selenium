858703d29093d55ca4b31ef3c55fc8b9b7335291
Juan Camilo Mejía Montoya <j66cam@gmail.com>
2024-10-31 16:46:16 -0500

    feat(ExtentReport): Extent Report implementation

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -2,6 +2,9 @@ package hooks;
 
 import base.Browser;
 import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
 public class Hooks {
@@ -9,8 +12,12 @@ public class Hooks {
 	public static WebDriver driver;
 
 	@After()
-	public void tearDown() {
+	public void tearDown(Scenario scenario) {
 		driver = Browser.getDriver();
+		if (scenario.isFailed()) {
+			final byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+			scenario.attach(screenshot, "image/png", scenario.getName());
+		}
 		if (driver != null) driver.quit();
 	}
 }
