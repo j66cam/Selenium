84ec27e632d4f1b3c4bc76dd84606328558dee18
Juan Camilo Mejía Montoya <j66cam@gmail.com>
2024-11-05 11:44:00 -0500

    feat(Allure Report): Initial set up

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -2,6 +2,9 @@ package hooks;
 
 import base.Browser;
 import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
 public class Hooks {
@@ -9,8 +12,16 @@ public class Hooks {
 	public static WebDriver driver;
 
 	@After()
-	public void tearDown() {
+	public void tearDown(Scenario scenario) {
 		driver = Browser.getDriver();
+		try {
+			if (scenario.isFailed()) {
+				byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+				scenario.attach(screenshot, "img/png", scenario.getName());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		if (driver != null) driver.quit();
 	}
 }
